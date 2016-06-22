684c35264634c8a3165567233e07d82c1caddefb
Mohammed Matar <mohammedmatar@yandex.com>
2016-06-22 17:07:00 +0000

    Launching our first BETA release - Ship it

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -11,7 +11,7 @@ module.exports = {
   // Server port
   port:   process.env.OPENSHIFT_NODEJS_PORT ||
           process.env.PORT ||
-          80,
+          8080,
 
   // MongoDB connection options
   mongo: {
@@ -19,6 +19,7 @@ module.exports = {
           process.env.MONGOHQ_URL ||
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
-          'mongodb://crossover-6762:gFXnMQwwb1c7A_ehGa5p@crossover-6762.mongo.dbs.appsdeck.eu:30388/crossover-6762'
+          'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/'
+          // 'mongodb://crossover-6762:gFXnMQwwb1c7A_ehGa5p@crossover-6762.mongo.dbs.appsdeck.eu:30388/crossover-6762'
   }
 };
--- server/config/express.js
+++ server/config/express.js
@@ -62,7 +62,7 @@ export default function(app) {
     resave: false,
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
-      db: 'moka'
+      db: 'crossover'
     })
   }));
 
